7e4536851045690d1ddd426bcb300c0b6bca7d16
gongchangco <gongchie@gmail.com>
2023-01-17 14:21:32 -0500

    Update project

--- vue-tailwindcss-blog/src/views/Home.vue
+++ vue-tailwindcss-blog/src/views/Home.vue
@@ -1,14 +1,34 @@
 <script setup>
-    import store from '../store.js'
+    import { store } from '../store.js'
+    import supabase from '../supabase'
 
+    const getWordsNum = (str) => (str.split(' '). length)
+
+    const fetchPosts = async () => {
+
+        let { data: posts, error } = await supabase
+        .from('posts')
+        .select
+
+        if (error) throw new Error(error)
+
+        store.posts = posts
+    }
+
+    fetchPosts()
 </script>
 
 <template>
     <div class="Home">
         <main class="container">
-            <div class="PostItem" v-for="item, itemIndex in store.posts" :key="itemIndex" @click="$router.push(`/post/${item.id}`)">
-                <h1>{{ item.title }}</h1>
-                <p>{{ item.description }}</p>
+            <div v-if="store.posts.length < 1">
+                There is no posts.
+            </div>
+            <div v-else>
+                <div class="PostItem" v-for="item, itemIndex in store.posts" :key="itemIndex" @click="$router.push(`/post/${item.id}`)">
+                    <h1>{{ item.title }}</h1>
+                    <p>{{ item.description }}</p>
+                </div>
             </div>
         </main>
     </div>
--- vue-tailwindcss-blog/src/views/Post.vue
+++ vue-tailwindcss-blog/src/views/Post.vue
@@ -1,15 +1,36 @@
 <script setup>
+    import { reactive } from 'vue'
     import { useRoute } from 'vue-router'
-    import store from '../store'
+    import { store } from '../store'
+    import supabase from '../supabase';
 
     const route = useRoute()
 
-    const post = store.posts.find(x => x.id === parseInt(route.params.id))
+    let post = reactive({})
+
+    const fetchPost = async (id) => {
+        const found = store.posts.find(x => x.id === parseInt(route.params.id))
+
+        if (found) {
+            return
+        }
+
+        let { data, error } = await supabase
+        .from('posts')
+        .select('*')
+        .eq('id', id)
+        .single()
+
+        if (error) throw new Error(error)
+
+        Object.assign(post, data)
+    }
+
+    fetchPost(route.params.id)
 </script>
 
 <template>
     <div class="Post">
-        
         <h1>{{ post.title }}</h1>
         <p>{{ post.date }}</p>
         <p>{{ post.description }}</p>        
