e55edc563b58cc865be9525129400c87c9dfc6bc
gongchangco <gongchie@gmail.com>
2023-02-13 22:07:38 -0500

    Create new project

---
+++ youtube-clone-react/src/components/Videos.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Stack, Box } from "@mui/material"
+
+const Videos = ({ videos, direction }) => {
+    return (
+        <div>Videos</div>
+    )
+}
+
+export default Videos
